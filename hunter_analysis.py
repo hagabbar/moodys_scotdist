923887a25d898dd1d2f386e2c2f4f25447afbc58
hagabbar <hunter.gabbard@gmail.com>
2019-03-26 18:07:44 +0000

    can now choose time and parameters

--- hunter_analysis.py
+++ hunter_analysis.py
@@ -5,17 +5,22 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-num_pars=5
-norm=False # normalize values to have a maximum of 1
+norm=False # normalize values to have a maximum of 1 if True
+num_trials=5000 # number of trials to use
 
 # read in csv file
 data_pd = pd.read_csv("Edi_Uni_Moodys_MonteCarlo.csv", index_col=False)
 
-#label_idx = [0,1,2]
+# choose your time and parameters here
+# label is parameter and time is time
+label_idx = list(np.arange(start=25,stop=51,step=1))
+# label_idx = [42,46]
+time_idx = list(np.arange(start=0,stop=51,step=1))
+# time_idx = [0,1]
 
 # make a corner plot
 data_np = data_pd.values
-labels = data_np[:num_pars,1]
+labels = data_np[label_idx,1]
 data_np_new = []
 for i in range(len(data_np)):
     data_np_new.append(pd.to_numeric(data_np[i,2:]))
@@ -29,20 +34,18 @@ if norm == True:
         #data_np[:,i] = -1 + 2.*(data_np[:,i] - min(data_np[:,i])) / (max(data_np[:,i]) - min(data_np[:,i]))
         #data_np[:,i] = 2*( (data_np[:,i]- np.min(data_np[:,i])) / (np.max(data_np[:,i]) - np.min(data_np[:,i])) ) - 1
 
-# get trails data
-num_trials=5000
+# get requested data
 cnt=0
 for i in range(num_trials):
     if i == 0:
-        corner_data = data_np[cnt:num_pars,:].T
-        cnt+=59*2
+        corner_data = data_np[label_idx,:]
+        corner_data = corner_data[:,time_idx].T
+        cnt+=59
         continue
-    corner_data = np.vstack([corner_data,data_np[59*i:cnt-(59-num_pars),:].T])
+    corner_data = np.vstack([corner_data,data_np[(np.array(label_idx)+cnt),:][:,time_idx].T])
     cnt+=59
 
-#from pandas.plotting import scatter_matrix
-#scatter_matrix(pd.DataFrame(corner_data), alpha = 0.2, figsize = (6, 6), diagonal = 'kde')
-
+# make corner plot
 figure = corner.corner(corner_data, quantiles=(0.16,0.84), labels=labels,label_kwargs=dict(fontsize='9'))
 plt.savefig('corner_result.pdf')
 plt.close()
